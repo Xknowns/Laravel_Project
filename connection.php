726678cdcc8ac80291fd16558cfa41f251770199
Xknowns <165246990+Xknowns@users.noreply.github.com>
2025-07-16 10:49:48 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$db_host ="";
+$db_user ="";
+$db_password ="";
+$db_name ="";
+
+try {
+
+    $db = new PDO("mysql:host=($db_host);db_name=($db_name)", $db_user,$db_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+
+?>
