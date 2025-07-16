8dbc93a268ad92037dcf8908414192be66ab2544
Xknowns <165246990+Xknowns@users.noreply.github.com>
2025-07-16 12:41:23 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,23 +1,23 @@
 <?php
 
-
-$db_host ="";
-$db_user ="";
+// Instead of 'localhost' try:
+//$db_host = "localhost";
+$db_host ="localhost";
+$db_user ="root";
 $db_password ="";
-$db_name ="";
+$db_name ="users";
 
 try {
 
-    $db = new PDO("mysql:host=($db_host);db_name=($db_name)", $db_user,$db_password);
+    $db = new PDO("mysql:host={$db_host};db_name={$db_name}", $db_user,$db_password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
+    echo "Connected successfully";
 
 
 }
 catch(PDOException $e) {
     echo $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }
 
 
