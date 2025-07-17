6baf12e1faceb350ee1cc90d4282df9fe486f277
Xknowns <165246990+Xknowns@users.noreply.github.com>
2025-07-17 15:03:59 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@ $db_name ="users";
 
 try {
 
-    $db = new PDO("mysql:host={$db_host};db_name={$db_name}", $db_user,$db_password);
+    $db = new PDO("mysql:host={$db_host};dbname={$db_name}", $db_user,$db_password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 
